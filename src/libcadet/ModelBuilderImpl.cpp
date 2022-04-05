4cdd48fca50b7910860680e617646c56006a26fe
Jan Breuer <j.m.br@gmx.de>
2022-04-05 21:42:54 +0200

    outsource mixed DG/FV GRM implementation to separate unit operation

--- src/libcadet/ModelBuilderImpl.cpp
+++ src/libcadet/ModelBuilderImpl.cpp
@@ -35,6 +35,7 @@ namespace cadet
 
 		void registerGeneralRateModel(std::unordered_map<std::string, std::function<IUnitOperation*(UnitOpIdx)>>& models);
 		void registerGeneralRateModelDG(std::unordered_map<std::string, std::function<IUnitOperation*(UnitOpIdx)>>& models);
+		void registerGeneralRateModelDGFV(std::unordered_map<std::string, std::function<IUnitOperation*(UnitOpIdx)>>& models);
 		void registerLumpedRateModelWithPores(std::unordered_map<std::string, std::function<IUnitOperation*(UnitOpIdx)>>& models);
 		void registerLumpedRateModelWithPoresDG(std::unordered_map<std::string, std::function<IUnitOperation*(UnitOpIdx)>>& models);
 		void registerLumpedRateModelWithoutPores(std::unordered_map<std::string, std::function<IUnitOperation*(UnitOpIdx)>>& models);
@@ -63,6 +64,7 @@ namespace cadet
 		model::registerOutletModel(_modelCreators);
 		model::registerGeneralRateModel(_modelCreators);
 		model::registerGeneralRateModelDG(_modelCreators);
+		model::registerGeneralRateModelDGFV(_modelCreators);
 		model::registerLumpedRateModelWithPores(_modelCreators);
 		model::registerLumpedRateModelWithPoresDG(_modelCreators);
 		model::registerLumpedRateModelWithoutPores(_modelCreators);
