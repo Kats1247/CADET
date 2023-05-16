de3ff3ab768dd176d3fec85a5b5c5074ad3e3a03
Jan Breuer <j.m.br@gmx.de>
2023-05-16 09:15:40 +0200

    AD Jacobian seeding for LRMP

--- src/libcadet/model/parts/ConvectionDispersionOperatorDG.hpp
+++ src/libcadet/model/parts/ConvectionDispersionOperatorDG.hpp
@@ -141,8 +141,8 @@ public:
 	// @todo use more efficient seed vectors. currently, we treat the jacobian as banded, but the pattern is actually more sparse when multiple components are considered
 	// (note that active type directions are limited)
 	// We have different jacobian structure for exact integration and collocation DG scheme, i.e. we need different seed vectors
-	// collocation DG: 2 * N_n * (N_c + N_q) + 1 = total bandwidth (main diagonal entries maximally depend on the next and last N_n liquid phase entries of same component)
-	//    ex. int. DG: 4 * N_n * (N_c + N_q) + 1 = total bandwidth (main diagonal entries maximally depend on the next and last 2*N_n liquid phase entries of same component)
+	// collocation DG: 2 * N_n * (strideNode) + 1 = total bandwidth (main diagonal entries maximally depend on the next and last N_n liquid phase entries of same component)
+	//    ex. int. DG: 4 * N_n * (strideNode) + 1 = total bandwidth (main diagonal entries maximally depend on the next and last 2*N_n liquid phase entries of same component)
 	int requiredADdirs() const CADET_NOEXCEPT { return (_exactInt) ? 4 * _nNodes * strideColNode() + 1 : 2 * _nNodes * strideColNode() + 1; }
 
 
